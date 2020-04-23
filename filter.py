d90c8490bc860b5f1eb066f7870cdb6f81ca7c59
cslev <cs.lev@gmx.com>
2020-04-23 15:48:20 +0800

    Fix and update

--- filter.py
+++ filter.py
@@ -8,12 +8,12 @@ import argparse
 parser = argparse.ArgumentParser(description="Python-based DNS filter!")
 
 parser.add_argument('-f', '--filter-dns', action="store_true", dest="filter_dns" , help="Enable pure DNS based filtering (Default: False)")
-parses.set_defaults(filter_dns=False)
+parser.set_defaults(filter_dns=False)
 
-parser.add_argument('-d', '--domain', action="store", default="index.hu", type=string, dest="domain_to_filter" , help="Specify the domain to filter for with DNS (Default: index.hu)! Use only with -f/--filter-dns option!")
+parser.add_argument('-d', '--domain', action="store", default="index.hu", type=str, dest="domain_to_filter" , help="Specify the domain to filter for with DNS (Default: index.hu)! Use only with -f/--filter-dns option!")
 
 parser.add_argument('-g', '--filter-doh', action="store_true", dest="filter_doh" , help="Enable DoH-based filtering - This will drop all packets that looks like a DoH query (Default: False)")
-parses.set_defaults(filter_doh=False)
+parser.set_defaults(filter_doh=False)
 
 results = parser.parse_args()
 
@@ -22,12 +22,12 @@ domain_to_filter=results.domain_to_filter
 filter_doh=results.filter_doh
 
 print("Configuration:")
-if(!filter_dns):
+if(not filter_dns):
     print("BLIND FORWARDING MODE")
 else:
     print("The following domain will be blocked via pure DNS: {}".format(domain_to_filter))
 
-if(!filter_doh):
+if(not filter_doh):
     print("DNS-over-HTTPS is allowed")
 else:
     print("DNS-over-HTTPS needs to be blocked")
@@ -41,36 +41,43 @@ def filter_packets(packet):
         #packets coming from the user
 		if(packet[0][1].dst != "10.10.10.101" and packet[0][0].src != new_mac):
             #setting the source MAC to a pseudo-random one
-            packet[0][0].src=new_mac
+			packet[0][0].src=new_mac
 
 			### ------- HERE COMES ANY FILTERING --------- ###
-            ##------- NO FILTER ---------
-            if(!filter_dns and !filter_doh):
-                # FORWARD everything else
+			##------- NO FILTER ---------
+			if(not filter_dns and not filter_doh):
+				# FORWARD everything else
 				sendp(packet, iface="eth0")
-                print("FORWARDING...")
-            ## -------- FILTERING -------
-            else:
-                ##  ============ PURE DNS FILTERING ============
-                if(filter_dns):
-        			if packet.haslayer(DNS):
-        				if packet.qdcount > 0 and isinstance(packet.qd, DNSQR):
-        					#print("DNS query")
-        					name = packet[DNSQR].qname
-        					#print(name)
-        					if(domain_to_filter in str(name)):
-        						# DNS queries looking for domain_to_filter will be dropped
-        						print("Gotcha {}  - DROP".format(domain_to_filter))
-        					else:
-        						# FORWARD every other DNS queries
-        						sendp(packet, iface="eth0")
-                ## ------- PURE DNS FILTERING END ------
+				print("FORWARDING...")
+			## -------- FILTERING -------
+			else:
+				##  ============ PURE DNS FILTERING ============
+				if(filter_dns):
+					if packet.haslayer(DNS):
+						if packet.qdcount > 0 and isinstance(packet.qd, DNSQR):
+							#print("DNS query")
+							name = packet[DNSQR].qname
+							#print(name)
+							if(domain_to_filter in str(name)):
+								# DNS queries looking for domain_to_filter will be dropped
+								print("Gotcha {}  - DROP".format(domain_to_filter))
+							else:
+								# FORWARD every other DNS queries
+								sendp(packet, iface="eth0")
+								print("FORWARDING DNS query ({})...".format(name))
+					elif(not filter_doh):
+						# FORWARD every IP packet which is not DNS queries
+						sendp(packet, iface="eth0")
+						print("FORWARDING NON-DNS packet")
+				## ------- PURE DNS FILTERING END ------
 
-                ## ============  DNS-over-HTTPS filtering ===========
-                if(filter_doh):
-                    pass
-                ## ----- DNS-over-HTTPS filtering END ----------------
+				## ============  DNS-over-HTTPS filtering ===========
+				if(filter_doh):
+					sendp(packet, iface="eth0")
+					print("DoH Filter not implemented...FORWARDING")
+				## ----- DNS-over-HTTPS filtering END ----------------
 			####============== FILTERING ENDS ============####
+			
 
 		elif(packet[0][1].dst == "10.10.10.101"):
 			# This host was the destination
